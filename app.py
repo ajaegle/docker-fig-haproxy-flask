fce5f5279b0159641631dd3d47856d05b6eab010
ajaegle <mail@ajaegle.de>
2014-11-25 23:21:38 +0100

    duplicate web container for parallel setup, distinguished by id

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World! I have been opened %s times.' % redis.get('hits')
+    backend_id =  os.environ['AJ_BACKEND']
+    return 'Hello World! I have been opened %s times. \nBackend ID: %s' % (redis.get('hits'), backend_id)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
